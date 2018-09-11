77d33ec312f3608be126d750cad30f8d12bb238d
matheus-lagreca <matheus.lagreca@gmail.com>
2018-09-11 12:12:44 -0300

    Add files via upload

---
+++ src/Parede.java
@@ -0,0 +1,33 @@
+
+public class Parede {
+	private int x;
+	private int y;
+	
+	public Parede(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int colisoes(Parede [] lista){
+		for (int i = 0; i < 4; i++) {
+			
+		}
+		return 0;
+	}
+	public int extremos(Parede [] lista)
+	{
+		
+		return 0;
+	}
+}
