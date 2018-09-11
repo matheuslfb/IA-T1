18aecdf8210be1f2d9a46f6cf6328297a2717d6c
matheus-lagreca <matheus.lagreca@gmail.com>
2018-09-11 12:11:50 -0300

    Update Gerador.java

--- src/Gerador.java
+++ src/Gerador.java
@@ -34,99 +34,189 @@ public class Gerador {
 
 		int[][] amigo = new int[10][10];
 		clone(amigo, base);
-		int verhor = ran.nextInt(1);
-		int xAmigo = ran.nextInt(10);
-		int yAmigo = ran.nextInt(10);
-		// 0 = vertical
-		// 1 = horizontal
-		// Verificar qual variacao do paredao
-		// em cima
-		if (amigo[0][0] == 1 && amigo[0][9] == 1) {
-			// vertical
-			if (verhor == 0) {
-				// 0-4 6-9
-				if (xAmigo <= 4) {
+		// FOR 4 vezes
+		for (int o = 0; o < 4; o++) {// gera 4 paredes aleatorias
 
-				} else if (xAmigo >= 6) {
+			int verhor = ran.nextInt(1);
+			int xAmigo = ran.nextInt(10);
+			int yAmigo = ran.nextInt(10);
+			// 0 = vertical
+			// 1 = horizontal
+			// Verificar qual variacao do paredao
+			// em cima
+			if (amigo[0][0] == 1 && amigo[0][9] == 1) {
+				// vertical
+				if (verhor == 0) {
+					// 0-4 6-9
+					if (xAmigo <= 4) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
 
+					} else if (xAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo--;
+						}
+					}
+
+					// CASO 5 faz pra cima
+					// horizontal
+				} else {
+					// 0-3 6-9
+					if (yAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					} else if (yAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo--;
+						}
+					}
+					// CASO 4 ou 5 faz pra Direita pq eh sucesso
 				}
-				// CASO 5 faz pra cima
-				// horizontal
-			} else {
-				// 0-3 6-9
-				if (yAmigo <= 3) {
-
-				} else if (yAmigo >= 6) {
-
+			}
+			// direita
+			if (amigo[0][9] == 1 && amigo[1][9] == 1) {
+				if (verhor == 0) {
+					// 0-3 6-9
+					if (xAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					} else if (xAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					}
+					// CASO 4 ou 5 faz pra cima
+					// horizontal
+				} else {
+					// 0-3 5-9
+					if (yAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					} else if (yAmigo >= 5) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					}
+					// CASO 4 faz pra Direita pq eh sucesso
 				}
-				// CASO 4 ou 5 faz pra Direita pq eh sucesso
+
+			}
+			// esquerda
+			if (amigo[0][0] == 1 && amigo[1][0] == 1) {
+				if (verhor == 0) {
+					// 0-3 6-9
+					if (xAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					} else if (xAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					}
+					// CASO 4 ou 5 faz pra cima
+				} else {
+					// 0-4 6-9
+					if (yAmigo <= 4) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					} else if (yAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					}
+					// CASO 4 ou 5 faz pra Direita pq eh sucesso
+				}
+			}
+			// baixo
+			if (amigo[9][0] == 1 && amigo[9][1] == 1) {
+				if (verhor == 0) {
+					// 0-3 5-9
+					if (xAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					} else if (xAmigo >= 5) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							xAmigo++;
+						}
+					}
+					// CASO 4 ou 5 faz pra cima
+				} else {
+					// 0-3 6-9
+					if (yAmigo <= 3) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					} else if (yAmigo >= 6) {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo--;
+						}
+					} else {
+						for (int i = 0; i < 5; i++) {
+							amigo[xAmigo][yAmigo] = 2;
+							yAmigo++;
+						}
+					}
+					// CASO 4 ou 5 faz pra Direita pq eh sucesso
+				}
 			}
 		}
-		// direita
-		if (amigo[0][9] == 1 && amigo[1][9] == 1) {
-			if (verhor == 0) {
-				// 0-3 6-9
-				if (xAmigo <= 3) {
-
-				} else if (xAmigo >= 6) {
-
-				}
-				// CASO 4 ou 5 faz pra cima
-				// horizontal
-			} else {
-				// 0-3 5-9
-				if (yAmigo <= 3) {
-
-				} else if (yAmigo >= 5) {
-
-				}
-				// CASO 4  faz pra Direita pq eh sucesso
-			}
-
-		}
-		// esquerda
-		if (amigo[0][0] == 1 && amigo[1][0] == 1) {
-			if (verhor == 0) {
-				// 0-3 6-9
-				if (xAmigo <= 3) {
-
-				} else if (xAmigo >= 6) {
-
-				}
-				// CASO 4 ou 5 faz pra cima
-			} else {
-				// 0-4 6-9
-				if (yAmigo <= 4) {
-
-				} else if (yAmigo >= 6) {
-
-				}
-				// CASO 4 ou 5 faz pra Direita pq eh sucesso
-			}
-		}
-		// baixo
-		if (amigo[9][0] == 1 && amigo[9][1] == 1) {
-			if (verhor == 0) {
-				// 0-3 5-9
-				if (xAmigo <= 3) {
-
-				} else if (xAmigo >= 5) {
-
-				}
-				// CASO 4 ou 5 faz pra cima
-			} else {
-				// 0-3 6-9
-				if (yAmigo <= 3) {
-
-				} else if (yAmigo >= 6) {
-
-				}
-				// CASO 4 ou 5 faz pra Direita pq eh sucesso
-			}
-		}
-		//dps do for ANALISAR
-		//SE DEU CACA usar tratamento da folha
-
+		//verificar colisoes e extremos
+		// SE DEU CACA usar tratamento da folha
+		
+		
+	}
+	public int colisoes(int[][]base){
+		
+		return 0;
+		
 	}
 
 	public void clone(int[][] base, int[][] amigo) {
