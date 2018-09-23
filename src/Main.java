95d2ba77475a0f6695c34203acf432bd4a327c29
matheus-lagreca <matheus.lagreca@gmail.com>
2018-09-23 16:24:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Ambiente a = new Ambiente();
-        Gerador g = new Gerador();
+		Ambiente a = new Ambiente();
+		Gerador g = new Gerador();
 
-        g.GeraParedao(a.base);
-        //g.GeraParede(a.base);
-        a.printAmbiente(a.base);
-    }
+		g.GeraParedao(a.base);
+		// g.GeraParede(a.base);
+		g.GeraParede2(a.base);
+
+		a.printAmbiente(a.base);
+	}
 }
