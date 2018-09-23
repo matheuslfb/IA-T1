ad782c04d9d5bd0f90e51a7eff5fd317b8be0cdf
matheus-lagreca <matheus.lagreca@gmail.com>
2018-09-23 17:45:22 -0300

    Update Gerador.java

--- src/Gerador.java
+++ src/Gerador.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Gerador {
@@ -42,6 +43,7 @@ public class Gerador {
 			// gera parede usando geraParedeCB
 			// ele so devolte a lista tem q pegar e colocar
 			// ignora o tratamento de colisoes por enquanto
+
 		} else {
 			// gera parede usando geraParedeCB
 
@@ -50,12 +52,109 @@ public class Gerador {
 		clone(amigo, base);
 	}
 
-	public int colisoes(int[][] base) {
+	public void GeraPosSacoMoedas(int[][] base) {
 
-		return 0;
+		// espalha
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		for (int i = 0; i < 16; i++) {
+			do {
+				posX = ran.nextInt(10);
+				posY = ran.nextInt(10);
+			} while (base[posX][posY] != 0);
+			// posicao
+			// uma
+			// vazia
+			base[posX][posY] = 4;
+		}
 
 	}
 
+	public void geraburacos(int[][] base) {
+		// espalha
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		for (int i = 0; i < 16; i++) {
+			do {
+				posX = ran.nextInt(10);
+				posY = ran.nextInt(10);
+			} while (base[posX][posY] != 0);
+			// posicao
+			// uma
+			// vazia
+			base[posX][posY] = 3;
+		}
+
+	}
+
+	public void geraBaus(int[][]base) {
+		//achar paredao
+		//achar 4 lugares livres no paredao
+		//colocar 4 baus
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		if(base[0][0] ==1 && base[0][9]==1) {
+			//cima
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = 1;
+					posY = ran.nextInt(10);
+				} while (base[posX][posY] != 0);
+				
+				base[posX][posY] = 5;
+			}
+		}
+		else if(base[9][0] ==1 && base[9][9]==1) {
+			//baixo
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = 8;
+					posY = ran.nextInt(10);
+				} while (base[posX][posY] != 0);
+				
+				base[posX][posY] = 5;
+			}
+		}
+		else if(base[0][0] ==1 && base[9][0]==1) {
+			//esquerda
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = ran.nextInt(10);
+					posY = 1;
+				} while (base[posX][posY] != 0);
+				
+				base[posX][posY] = 5;
+			}
+		}
+		else if(base[0][9] ==1 && base[9][9]==1) {
+			//direita
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = ran.nextInt(10);
+					posY = 8;
+				} while (base[posX][posY] != 0);
+				
+				base[posX][posY] = 5;
+			}
+		}
+		
+	}
+	public void geraSpawn(int base[][]) {
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		do {
+			posX = ran.nextInt(10);
+			posY = ran.nextInt(10);
+		} while (base[posX][posY] != 0);
+		
+		base[posX][posY] = 7;
+	}
+	
+	
 	public void clone(int[][] base, int[][] amigo) {
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
