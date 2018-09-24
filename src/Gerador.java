16330f7ee9e7f1fdcfa2f947b336add57187bc3f
matheus-lagreca <matheus.lagreca@gmail.com>
2018-09-24 15:19:57 -0300

    Update Gerador.java

--- src/Gerador.java
+++ src/Gerador.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 public class Gerador {
 	Random ran = new Random();
 
@@ -12,7 +10,7 @@ public class Gerador {
 			for (int i = 0; i < base.length; i++) {
 				base[0][i] = 1;
 			}
-			base[0][ran.nextInt(base.length)] = 6; // garante que a saida vai estar em no paredÃƒÂ£o
+			base[0][ran.nextInt(base.length)] = 6; // garante que a saida vai estar em no paredÃ£o
 		}
 		if (op == 1) {// preenche a direita
 			for (int i = 0; i < base.length; i++) {
@@ -46,19 +44,19 @@ public class Gerador {
 		int pos = 0;
 		if ((amigo[0][0] == 1 && amigo[0][9] == 1) || (amigo[9][0] == 1 && amigo[9][1] == 1)) {
 			lista = p.geraParedeCB();
-			while(count<4) {
+			while (count < 4) {
 				if (lista.get(pos).getyStart() == lista.get(pos).getyEnd()) {
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+1][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+2][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+3][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+4][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(0).getXStart()][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(1).getXStart() + 1][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(2).getXStart() + 2][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(3).getXStart() + 3][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(4).getXStart() + 4][lista.get(pos).getyStart()] = 2;
 				} else {
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+1] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+2] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+3] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+4] = 2;
+					amigo[lista.get(0).getXStart()][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(1).getXStart()][lista.get(pos).getyStart() + 1] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 2] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 3] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 4] = 2;
 				}
 				pos++;
 				count++;
@@ -67,32 +65,138 @@ public class Gerador {
 			lista = p.geraParedeED();
 			count = 0;
 			pos = 0;
-			while(count<4) {
+			while (count < 4) {
 				if (lista.get(pos).getyStart() == lista.get(pos).getyEnd()) {
 					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+1][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+2][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+3][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()+4][lista.get(pos).getyStart()] = 2;
+
+					amigo[lista.get(pos).getXStart() + 1][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(pos).getXStart() + 2][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(pos).getXStart() + 3][lista.get(pos).getyStart()] = 2;
+					amigo[lista.get(pos).getXStart() + 4][lista.get(pos).getyStart()] = 2;
 				} else {
 					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+1] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+2] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+3] = 2;
-					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart()+4] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 1] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 2] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 3] = 2;
+					amigo[lista.get(pos).getXStart()][lista.get(pos).getyStart() + 4] = 2;
 				}
 				pos++;
 				count++;
 			}
 		}
-
-		clone(amigo, base);
 	}
 
-	public int colisoes(int[][] base) {
+	public void GeraPosSacoMoedas(int[][] base) {
 
-		return 0;
+		// espalha
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		for (int i = 0; i < 16; i++) {
+			do {
+				posX = ran.nextInt(10);
+				posY = ran.nextInt(10);
+			} while (base[posX][posY] != 0);
+			// posicao
+			// uma
+			// vazia
+			base[posX][posY] = 4;
+		}
 
+	}
+
+	public void geraburacos(int[][] base) {
+		// espalha
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		for (int i = 0; i < 5; i++) {
+			do {
+				posX = ran.nextInt(10);
+				posY = ran.nextInt(10);
+				if (posX > 8) {
+					posX = 8;
+				}
+				if (posX < 2) {
+					posX = 2;
+				}
+				if (posY > 8) {
+					posY = 8;
+				}
+				if (posY < 2) {
+					posY = 2;
+				}
+			} while (base[posX][posY] != 0 && (base[posX + 1][posY] == 3 || base[posX - 1][posY] == 3
+					|| base[posX][posY + 1] == 3 || base[posX][posY - 1] == 3));
+			// posicao
+			// uma
+			// vazia
+			base[posX][posY] = 3;
+		}
+
+	}
+
+	public void geraBaus(int[][] base) {
+		// achar paredao
+		// achar 4 lugares livres no paredao
+		// colocar 4 baus
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		if (base[0][0] == 1 && base[0][9] == 1) {
+			// cima
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = 1;
+					posY = ran.nextInt(10);
+				} while (base[posX][posY] != 0);
+
+				base[posX][posY] = 5;
+			}
+		} else if (base[9][0] == 1 && base[9][9] == 1) {
+			// baixo
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = 8;
+					posY = ran.nextInt(10);
+				} while (base[posX][posY] != 0);
+
+				base[posX][posY] = 5;
+			}
+		} else if (base[0][0] == 1 && base[9][0] == 1) {
+			// esquerda
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = ran.nextInt(10);
+					posY = 1;
+				} while (base[posX][posY] != 0);
+
+				base[posX][posY] = 5;
+			}
+		} else if (base[0][9] == 1 && base[9][9] == 1) {
+			// direita
+			for (int i = 0; i < 4; i++) {
+				do {
+					posX = ran.nextInt(10);
+					posY = 8;
+				} while (base[posX][posY] != 0);
+
+				base[posX][posY] = 5;
+			}
+		}
+
+	}
+
+	public void geraSpawn(int base[][]) {
+		Random ran = new Random();
+		int posX = 1;
+		int posY = 1;
+		do {
+			posX = ran.nextInt(10);
+			posY = ran.nextInt(10);
+		} while (base[posX][posY] != 0);
+
+		base[posX][posY] = 7;
 	}
 
 	public void clone(int[][] base, int[][] amigo) {
